b2a70d85ddec3a0b17a09ac56f5951d23bfe6e2f
Tony <tony.hu1213@gmail.com>
2018-06-17 09:55:11 -0700

    Remove redundant code

--- src/main/java/bitOperation/LintCode1046.java
+++ src/main/java/bitOperation/LintCode1046.java
@@ -34,9 +34,7 @@ public class LintCode1046 {
     if (number < 2) {
       return false;
     }
-    if (number == 2 || number == 3) {
-      return true;
-    }
+
     for (int i = 2; i * i <= number; i++) {
       if (number % i == 0) {
         return false;
