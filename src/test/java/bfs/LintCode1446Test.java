598f1cd8a72353854a7fde2e8ca772b1560462a4
Tony <tony.hu1213@gmail.com>
2018-06-24 19:55:25 -0700

    Modify the code coverage tool from cobertura to JaCoCo

--- src/test/java/bfs/LintCode1446Test.java
+++ src/test/java/bfs/LintCode1446Test.java
@@ -11,10 +11,12 @@ public class LintCode1446Test {
     int[][] test1 = new int[][]{{0, 0, 0}, {0, 0, 1}, {0, 1, 1}};
     int[][] test2 = new int[][]{{0,1,0,0,0},{0,0,0,1,0},{1,1,0,1,0},{1,1,1,1,0}};
     int[][] test3 = new int[][]{{0,1,1},{1,1,0},{1,1,0}};
+    int[][] test4 = new int[][]{{1,1,1}, {0, 0, 0}};
 
     LintCode1446 lintCode1446 = new LintCode1446();
     assertEquals(-1, lintCode1446.getBestRoad(test1));
     assertEquals(7, lintCode1446.getBestRoad(test2));
     assertEquals(-1, lintCode1446.getBestRoad(test3));
+    assertEquals(3, lintCode1446.getBestRoad(test4));
   }
 }
