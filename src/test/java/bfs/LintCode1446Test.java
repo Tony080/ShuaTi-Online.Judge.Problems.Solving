ee9ebf9b66d900a5e4fca4b700cbcb56acbcb2ae
Tony <tony.hu1213@gmail.com>
2018-06-24 19:59:14 -0700

    Improve code coverage.

--- src/test/java/bfs/LintCode1446Test.java
+++ src/test/java/bfs/LintCode1446Test.java
@@ -12,11 +12,13 @@ public class LintCode1446Test {
     int[][] test2 = new int[][]{{0,1,0,0,0},{0,0,0,1,0},{1,1,0,1,0},{1,1,1,1,0}};
     int[][] test3 = new int[][]{{0,1,1},{1,1,0},{1,1,0}};
     int[][] test4 = new int[][]{{1,1,1}, {0, 0, 0}};
+    int[][] test5 = new int[][]{{0,1,0,0,0},{0,0,0,1,0},{1,1,1,0,0},{1,1,1,1,1},{1,1,1,0,0}};
 
     LintCode1446 lintCode1446 = new LintCode1446();
     assertEquals(-1, lintCode1446.getBestRoad(test1));
     assertEquals(7, lintCode1446.getBestRoad(test2));
     assertEquals(-1, lintCode1446.getBestRoad(test3));
     assertEquals(3, lintCode1446.getBestRoad(test4));
+    assertEquals(10, lintCode1446.getBestRoad(test5));
   }
 }
