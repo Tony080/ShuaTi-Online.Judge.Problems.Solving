55b7066ea3c2c85a25331bd220f5d5846d03f417
Tony <tony.hu1213@gmail.com>
2018-06-30 17:31:51 -0700

    improve code coverage

--- src/test/java/array/LintCode1225Test.java
+++ src/test/java/array/LintCode1225Test.java
@@ -9,11 +9,16 @@ public class LintCode1225Test {
   @Test
   public void islandPerimeter() {
     int[][] test1 = new int[][]{{0,1,0,0},
-                                 {1,1,1,0},
-                                 {0,1,0,0},
-                                 {1,1,0,0}};
+                                {1,1,1,0},
+                                {0,1,0,0},
+                                {1,1,0,0}};
+    int[][] test2 = new int[][]{{0,1,0,0},
+                                {1,1,1,1},
+                                {0,1,0,0},
+                                {1,1,0,0}};
 
     LintCode1225 lintCode1225 = new LintCode1225();
     assertEquals(16, lintCode1225.islandPerimeter(test1));
+    assertEquals(18, lintCode1225.islandPerimeter(test2));
   }
 }
