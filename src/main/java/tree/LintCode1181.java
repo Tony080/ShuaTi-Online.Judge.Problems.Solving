b1e8f1edada09d5422bf31abbb89899cfb89d2fd
Tony <tony.hu1213@gmail.com>
2018-08-22 04:10:08 +0800

    Add LintCode1181

---
+++ src/main/java/tree/LintCode1181.java
@@ -0,0 +1,32 @@
+package tree;
+
+import bfs.LintCode726.TreeNode;
+//Link: https://www.lintcode.com/problem/diameter-of-binary-tree/description
+//Title: Diameter of Binary Tree
+//Difficulty: Easy
+public class LintCode1181 {
+  int max = 0;
+  public int diameterOfBinaryTree(TreeNode root) {
+    // write your code here
+    helper(root);
+    return max;
+  }
+
+  private int helper(TreeNode root) {
+    if (root == null || (root.left == null && root.right == null)) {
+      return 0;
+    }
+    int left = 0;
+    int right = 0;
+    if (root.left != null) {
+      left = helper(root.left) + 1;
+    }
+    if (root.right != null) {
+      right = helper(root.right) + 1;
+    }
+
+    int sum = left + right;
+    max = Math.max(sum, max);
+    return Math.max(left, right);
+  }
+}
---
+++ src/test/java/tree/LintCode1181Test.java
@@ -0,0 +1,13 @@
+package tree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LintCode1181Test {
+
+  @Test
+  public void diameterOfBinaryTree() {
+
+  }
+}
