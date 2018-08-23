d2ab40d5ed2c6bb245829e1cbfc85b4e623642cd
Tony <tony.hu1213@gmail.com>
2018-08-22 20:40:14 -0700

    Improve code coverage

--- src/test/java/string/LintCode1174Test.java
+++ src/test/java/string/LintCode1174Test.java
@@ -11,10 +11,12 @@ public class LintCode1174Test {
     int test1 = 12345;
     int test2 = 54321;
     int test3 = 1999999999;
+    int test4 = 1321;
 
     LintCode1174 lintCode1174 = new LintCode1174();
     assertEquals(12354, lintCode1174.nextGreaterElement(test1));
     assertEquals(-1, lintCode1174.nextGreaterElement(test2));
     assertEquals(-1, lintCode1174.nextGreaterElement(test3));
+    assertEquals(2113, lintCode1174.nextGreaterElement(test4));
   }
 }
